bf4bbbf09970c77267baad108224f4173cbce727
Jakub <stasiewski.jakub@gmail.com>
2023-02-07 22:59:46 +0100

    registration

--- chat/src/pages/Register.jsx
+++ chat/src/pages/Register.jsx
@@ -1,15 +1,38 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 import React from 'react'
 import avatar from '../photos/avatar.png'
 import {Link} from 'react-router-dom'
 
 
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+}
+
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
         <span className='logo'>Chat App</span>
         <span className='title'>Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type='text' placeholder='displat name'/>
           <input type='email'placeholder='email'/>
           <input type='password' placeholder='password'/>
