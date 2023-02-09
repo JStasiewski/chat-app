16de5c0a4ea13d98af7b6c2b137c16ff007e04b7
Jakub <stasiewski.jakub@gmail.com>
2023-02-09 17:54:38 +0100

    err handling for login site (1/2)

--- chat/src/pages/Login.jsx
+++ chat/src/pages/Login.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const errList = [
+    "",
+    "Fill Email",
+    "Fill Password",
+    "Wrong password or email"
+  ]
+
+  const handleSubmit = async (e)=> {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    if(email=="") {setErr(1); return false};
+    if(password=="") {setErr(2); return false};
+
+    try{
+      
+    }catch (err){
+      setErr(3);
+    }
+  }
+
   return (
     <div className='formContainer'>
       <div className='formWrapper'>
         <span className='logo'>Chat App</span>
         <span className='title'>Login</span>
-        <form>
-          <input type='email'placeholder='email'/>
-          <input type='password' placeholder='password'/>
+        <form onSubmit={handleSubmit}>
+          <input type='email'placeholder='email' style={{boxShadow: err===1 ? "inset 0px 0px 10px red":""}}/>
+          <input type='password' placeholder='password' style={{boxShadow: err===2 ? "inset 0px 0px 10px red":""}}/>
           <button>Login</button>
         </form>
         <p>No account? <Link to="/register">Register here</Link></p>
