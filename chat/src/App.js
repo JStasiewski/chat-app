932dc4b053685c34038642a305927bde5067d7f6
Jakub <stasiewski.jakub@gmail.com>
2023-02-09 17:44:14 +0100

    sent to login page if user is not logged in

--- chat/src/App.js
+++ chat/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route,} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate,} from "react-router-dom";
 import { AuthContext } from './context/AuthContext';
 import Register from './pages/Register';
 import Login from './pages/Login';
@@ -7,14 +7,24 @@ import Home from './pages/Home';
 import "./style.scss";
 
 function App() {
-
   const {currentUser} = useContext(AuthContext);
+
+  const ProtectRoute = ({childern}) => {
+    if(!currentUser){
+      return <Navigate to="/login"/>
+    }
+  }
+
   console.log(currentUser);
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home/>} />
+          <Route index element={
+            <ProtectRoute>
+              <Home/>
+            </ProtectRoute>
+            }/>
           <Route path="login" element={<Login/>} />
           <Route path="register" element={<Register/>} />
         </Route>
