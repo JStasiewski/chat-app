f867f308b29d0a79b2359ec8227b1b45a296c891
Jakub <stasiewski.jakub@gmail.com>
2023-02-09 17:39:22 +0100

    logging out

--- chat/src/components/Navbar.jsx
+++ chat/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { auth } from '../firebase'
+import { signOut } from 'firebase/auth'
 
 const Navbar = () => {
   return (
@@ -9,7 +11,7 @@ const Navbar = () => {
         <img src="https://media.istockphoto.com/id/609686580/pl/zdj%C4%99cie/dziewczyna-w-sankt-petersburgu.jpg?s=612x612&w=0&k=20&c=7jlJkRZydwXGJMfMM7uzpOo_T_X-5ooTJRv_4sk1Ub4=" alt="user" />
         <span>Jakub Stasiewski dupa dupa dupa dupa dupa</span>
         {/* <span>dupa</span> */}
-        <Link to="/login"><button>logout</button></Link>
+        <Link to="/login"><button onClick={()=>signOut(auth)}>logout</button></Link>
       </div>
     </div>
   )
