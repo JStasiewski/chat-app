ef5532d61fea21f99e28000280b81bc5032f77e8
Jakub <stasiewski.jakub@gmail.com>
2023-02-10 18:27:42 +0100

    sending message to firebase

--- chat/src/components/Input.jsx
+++ chat/src/components/Input.jsx
@@ -1,18 +1,62 @@
-import React from 'react'
+import { arrayUnion, doc, Timestamp, updateDoc } from 'firebase/firestore'
+import React, { useContext, useState } from 'react'
 import avatar from "../photos/avatar.png"
 import clip from "../photos/clip.png"
+import { v4 as uuid} from 'uuid'
+import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
+import { db, storage } from '../firebase'
+import { updateProfile } from 'firebase/auth'
+import { ChatContext } from '../context/ChatContext'
+import { AuthContext } from '../context/AuthContext'
 
 const Input = () => {
+  const [text,setText] = useState("")
+  const [img,setImg] = useState(null)
+
+  const { currentUser } = useContext(AuthContext)
+  const { data } = useContext(ChatContext)
+
+  const handleSend = async() => {
+    if(img) {
+
+      const storageRef = ref(storage,uuid());
+      const uploadTask = uploadBytesResumable(storageRef, img);
+      uploadTask.on(
+        (error) => {
+          // setErr(5);
+        }, 
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+            messages: arrayUnion({
+              id: uuid(),
+              text,
+              semderId: currentUser.uid,
+              date: Timestamp.now()
+            })
+          });
+        });
+    }else {
+      await updateDoc(doc((db),"chats", data.chatId),{
+        messages: arrayUnion({
+          id: uuid(),
+          text,
+          semderId: currentUser.uid,
+          date: Timestamp.now()
+        })
+      })
+    }
+  }
+
   return (
     <div className='input'>
-      <input type="text" placeholder='Type something...' />
+      <input type="text" placeholder='Type something...' onChange={e=>setText(e.target.value)}/>
       <div className="send">
         <img src={clip} alt="" />
-        <input type="file" style={{display:"none"}} id="file" />
+        <input type="file" style={{display:"none"}} id="file" onChange={e=>setImg(e.target.files[0])}/>
         <label htmlFor="file">
           <img src={avatar} alt="" />
         </label>
-        <button>Send</button>
+        <button onClick={handleSend}>Send</button>
       </div>
     </div>
   )
