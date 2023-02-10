73dd6f1279726b3fa4229a9ba8e0a44d18fad9f9
Jakub <stasiewski.jakub@gmail.com>
2023-02-10 02:15:40 +0100

    chat style and basic structure

--- chat/src/components/Chat.jsx
+++ chat/src/components/Chat.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Input from './Input'
+import Messages from './Messages'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>username</span>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
 
--- chat/src/components/Input.jsx
+++ chat/src/components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import avatar from "../photos/avatar.png"
+import clip from "../photos/clip.png"
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Type something...' />
+      <div className="send">
+        <img src={clip} alt="" />
+        <input type="file" style={{display:"none"}} id="file" />
+        <label htmlFor="file">
+          <img src={avatar} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
