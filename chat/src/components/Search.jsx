fa0ea38033b74a089c1a5302eec00fd3291d7025
Jakub <stasiewski.jakub@gmail.com>
2023-02-10 15:32:44 +0100

    search adding chats from search

--- chat/src/components/Search.jsx
+++ chat/src/components/Search.jsx
@@ -3,14 +3,22 @@ import {
   collection,
   query,
   where,
-  getDocs
+  getDocs,
+  doc,
+  getDoc,
+  setDoc,
+  serverTimestamp,
+  updateDoc
 } from "firebase/firestore";
 import { db } from "../firebase";
+import {AuthContext} from '../context/AuthContext'
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState(null);
   const [err, setErr] = useState(false);
+
+  const {currentUser} = useContext(AuthContext);
 
   const handleSearch = async() => {
     const q = query(collection(db, "users"), where("displayName", "==" , username));
@@ -30,16 +38,47 @@ const Search = () => {
     e.code === "Enter" && handleSearch();
   }
 
+  const handleSelect = async () => {
+    const combinedId = currentUser.uid > user.uid ? currentUser.uid + user.uid : user.uid + currentUser.uid;
+    
+    try{
+      const res = await getDoc(doc(db, "chats", combinedId));
+      if(!res.exists()){
+        await setDoc(doc(db,"chats", combinedId), { messages: [] });
+        
+        await updateDoc(doc(db,"userChats",currentUser.uid), {
+          [combinedId+".userInfo"]: {
+            uid:user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL
+          },
+          [combinedId+".date"]: serverTimestamp()
+        });
+
+        await updateDoc(doc(db,"userChats",currentUser.uid),{
+          [combinedId+".date"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+          },
+          [combinedId+".date"]: serverTimestamp()
+        });
+      }
+    } catch(err) {
+
+    }
+  }
+
   return (
     <div className='search'>
       <div className='searchForm'>
         <input type="text" placeholder='find friend' onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
       </div>
       {err && <span>User not found</span>}
-      {user && <div className='userChat'>
+      {user && (<div className='userChat' onClick={handleSelect}>
         <img src={user.photoURL} alt="user search" />
         <span>{user.displayName}</span>
-      </div>}
+      </div>)}
       <div style={{marginLeft: 10,marginRight: 10, borderTop: "1px solid white"}}></div>
     </div>
   )
